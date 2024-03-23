b0febffed7610a69ea33692327ac63a13580b020
Pablo Burgos <pabloburgosc@gmail.com>
2024-03-23 13:03:18 -0300

    typos

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const impuestoCliente = new Impuestos(500000, 25000);
 const cliente01 = new Cliente("Pablo Burgos", impuestoCliente);
 
 const impuestoAnual = cliente01.calcularImpuesto();
-console.log(`El impuesto anual para ${cliente01.nombre} es: ${impuestoAnual}`);
+console.log(
+  `El impuesto anual calculado para ${cliente01.nombre} es: ${impuestoAnual}`
+);
